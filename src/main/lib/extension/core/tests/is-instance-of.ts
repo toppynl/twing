8960b3cf3f75f361cde8a1406dec32d5b4e6a2ba
Peep van Puijenbroek <peep@toppy.nl>
2026-04-21 12:30:05 +0200

    fix: validate constructor argument in instanceof test

--- src/main/lib/extension/core/tests/is-instance-of.ts
+++ src/main/lib/extension/core/tests/is-instance-of.ts
@@ -1,9 +1,18 @@
 import {TwingCallable, TwingSynchronousCallable} from "../../../callable-wrapper";
 
+const checkInstanceOf = (value: any, constructor: any): boolean => {
+    if (typeof constructor !== 'function') {
+        throw new Error(
+            `The "instanceof" test expects a constructor function as its argument, got "${typeof constructor}".`
+        );
+    }
+    return value instanceof constructor;
+};
+
 export const isInstanceOf: TwingCallable<[value: any, constructor: any], boolean> = (_executionContext, value, constructor) => {
-    return Promise.resolve(value instanceof constructor);
+    return Promise.resolve(checkInstanceOf(value, constructor));
 };
 
 export const isInstanceOfSynchronously: TwingSynchronousCallable<[value: any, constructor: any], boolean> = (_executionContext, value, constructor) => {
-    return value instanceof constructor;
+    return checkInstanceOf(value, constructor);
 };
